fffede0e0269bd827d711e55bb8e87915197e87a
Harishadithyan <harishadithyan17@gmail.com>
2026-02-23 05:46:59 +0530

    Stack

---
+++ 4.Stacks/1.StackDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class StackDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        // Internally they were also arrays
+        stack.push(4);
+        stack.push(14);
+        stack.push(24);
+        stack.push(34);
+        stack.push(44);
+        stack.push(54);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        // Removing empty stack gives an error
+        System.out.println(stack.pop());
+    }
+}
